03144722e2b2adf60df4cde5dd06e768be34748a
Alek Hristov <alek.hristov@yahoo.com>
2018-04-19 12:27:33 +0300

    BackUpSystem.Services.Auth Unit Tests project added.

---
+++ BackUpTwitterApp/BackUpSystem.Services.Auth.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackUpSystem.Services.Auth.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

5bc22628ae7dfcc3d7a96aa7d672231962f53221
Alek Hristov <alek.hristov@yahoo.com>
2018-04-20 18:26:06 +0300

    Refactoring

--- BackUpTwitterApp/BackUpSystem.Utils/Contracts/IJsonUserReader.cs
+++
@@ -1,9 +0,0 @@
-﻿using BackUpSystem.DTO;
-
-namespace BackUpSystem.Utilities.Contracts
-{
-    public interface IJsonUserReader
-    {
-        object DeserializeUser(string jsonResponseData);
-    }
-}

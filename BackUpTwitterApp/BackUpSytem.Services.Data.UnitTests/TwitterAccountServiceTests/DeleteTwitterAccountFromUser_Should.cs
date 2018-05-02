e2526a151cfa0ca691303f3c165bcec926d7ef46
Alek Hristov <alek.hristov@yahoo.com>
2018-05-03 02:16:20 +0300

    Stage Ignored file to Source Control

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterAccountServiceTests/DeleteTwitterAccountFromUser_Should.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TwitterAccountServiceTests
+{
+    [TestClass]
+    public class DeleteTwitterAccountFromUser_Should
+    {
+    }
+}

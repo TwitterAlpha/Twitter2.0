4ac6d777d5201e9574c34e3d2027456529ab3c13
Alek Hristov <alek.hristov@yahoo.com>
2018-05-02 19:44:17 +0300

    TwitterService GetUserById Method tested

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterServiceTests/GetUserById_Should.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TwitterServiceTests
+{
+    [TestClass]
+    public class GetUserById_Should
+    {
+        [TestMethod]
+        public void GetUser_WhenInvokedWithValidParameter()
+        {
+            //Arrange
+            var authCreationServiceMock = new Mock<IOAuthCreationService>();
+            var jsonDeserializerMock = new Mock<IJsonObjectDeserializer>();
+
+            //Act
+            var twitterService = new TwitterService(authCreationServiceMock.Object, jsonDeserializerMock.Object);
+
+        }
+    }
+}

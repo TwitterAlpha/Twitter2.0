4e4f3cf5ea9df17b3b0b70614d5ff85700bb8f54
Alek Hristov <alek.hristov@yahoo.com>
2018-05-02 17:01:33 +0300

    Added AdminController

---
+++ BackUpTwitterApp/BackUpSystem.Web/Areas/Admin/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackUpSystem.Web.Areas.Admin.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var test = "test";
+
+
+
+
+
+
+            return View();
+        }
+    }
+}
---
+++ BackUpTwitterApp/BackUpSystem.Web/Areas/Admin/Controllers/UserManagementController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackUpSystem.Web.Areas.Admin.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            var test = "test";
+
+            return View();
+        }
+    }
+}

53901f5dcab4a146056a384968ea189903ac2d19
Alek Hristov <alek.hristov@yahoo.com>
2018-05-02 12:10:24 +0300

    Adding missing migrations

---
+++ BackUpTwitterApp/BackUpSystem.Data/Migrations/20180430161100_AddedTwoNewColumnsToTweetTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BackUpSystem.Data.Migrations
+{
+    public partial class AddedTwoNewColumnsToTweetTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserMentioned",
+                table: "Tweets",
+                newName: "TweetUrl");
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Tweets",
+                newName: "TweetAuthor");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TweetUrl",
+                table: "Tweets",
+                newName: "UserMentioned");
+
+            migrationBuilder.RenameColumn(
+                name: "TweetAuthor",
+                table: "Tweets",
+                newName: "AuthorId");
+        }
+    }
+}

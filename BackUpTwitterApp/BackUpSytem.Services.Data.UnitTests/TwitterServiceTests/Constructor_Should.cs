8cf1fccd9840132e99d6cead4d4bc413b9d01996
Alek Hristov <alek.hristov@yahoo.com>
2018-05-02 17:54:52 +0300

    TwitterService Constructor tested

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterServiceTests/Constructor_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TwitterServiceTests
+{
+    class Constructor_Should
+    {
+    }
+}
---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BackUpSytem.Services.Data.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

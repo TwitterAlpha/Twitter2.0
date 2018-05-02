8b48767c00130c43e6d878c425efa01ca5f7cd2a
Alek Hristov <alek.hristov@yahoo.com>
2018-05-02 17:55:26 +0300

    TwitterService constructor tested

--- BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterServiceTests/Constructor_Should.cs
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterServiceTests/Constructor_Should.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BackUpSystem.Services.Auth.Contracts;
+using BackUpSystem.Services.Data;
+using BackUpSystem.Services.Data.Contracts;
+using BackUpSystem.Utilities.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 
 namespace BackUpSytem.Services.Data.UnitTests.TwitterServiceTests
 {
-    class Constructor_Should
+    [TestClass]
+    public class Constructor_Should
     {
+        [TestMethod]
+        public void CreateInstance_WhenInvokedWithValidParameters() 
+        {
+            //Arrange
+            var authCreationServiceMock = new Mock<IOAuthCreationService>();
+            var jsonDeserializerMock = new Mock<IJsonObjectDeserializer>();
+
+            //Act
+            var twitterService = new TwitterService(authCreationServiceMock.Object, jsonDeserializerMock.Object);
+
+            //Assert
+            Assert.IsNotNull(twitterService);
+            Assert.IsInstanceOfType(twitterService, typeof(ITwitterService));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithNullAuthServiceParameter()
+        {
+            // Arrange
+            //var authCreationServiceMock = new Mock<IOAuthCreationService>();
+            var jsonDeserializerMock = new Mock<IJsonObjectDeserializer>();
+
+            // Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new TwitterService(null, jsonDeserializerMock.Object));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithNullJsonDeserializerParameter()
+        {
+            // Arrange
+            var authCreationServiceMock = new Mock<IOAuthCreationService>();
+            //var jsonDeserializerMock = new Mock<IJsonObjectDeserializer>();
+
+            // Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new TwitterService(authCreationServiceMock.Object, null));
+        }
     }
 }

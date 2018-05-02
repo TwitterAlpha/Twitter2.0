ee806115514a632cfb80045643e8ed744cad71ba
Alek Hristov <alek.hristov@yahoo.com>
2018-05-03 02:50:12 +0300

    Stage Ignored file to Source Control

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TweetServiceTests/Constructor_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TweetServiceTests
+{
+    class Constructor_Should
+    {
+    }
+}

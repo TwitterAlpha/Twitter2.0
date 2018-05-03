7b71e8a87c53cd246502e79f77490d49850af7c6
simeonovanton <djnoreaga@gmail.com>
2018-05-03 20:59:40 +0300

    userServise Tests added

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/Constructor_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
+{
+    class Constructor_Should
+    {
+    }
+}

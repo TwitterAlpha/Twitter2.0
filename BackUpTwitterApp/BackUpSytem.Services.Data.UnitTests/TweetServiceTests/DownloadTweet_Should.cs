b2750577c8275c56749814758770625df32969e6
Alek Hristov <alek.hristov@yahoo.com>
2018-05-03 13:37:20 +0300

    Stage Ignored file to Source Control

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TweetServiceTests/DownloadTweet_Should.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TweetServiceTests
+{
+    [TestClass]
+    public class DownloadTweet_Should
+    {
+    }
+}

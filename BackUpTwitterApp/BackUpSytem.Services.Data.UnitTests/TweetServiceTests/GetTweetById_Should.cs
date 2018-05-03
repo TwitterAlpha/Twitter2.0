f8721b9ef0a1972dae08f54599aeb49f376c6dec
Alek Hristov <alek.hristov@yahoo.com>
2018-05-03 04:13:32 +0300

    Stage Ignored files to Source Control

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TweetServiceTests/GetTweetById_Should.cs
@@ -0,0 +1,50 @@
+﻿using BackUpSystem.Data.Models;
+using BackUpSystem.Data.Repositories.Contracts;
+using BackUpSystem.DTO;
+using BackUpSystem.Services.Data;
+using BackUpSystem.Utilities.Contracts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Threading.Tasks;
+
+namespace BackUpSytem.Services.Data.UnitTests.TweetServiceTests
+{
+    [TestClass]
+    public class GetTweetById_Should
+    {
+        [TestMethod]
+        public async Task ReturnTweetDtoObject_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var tweetRepositoryMock = new Mock<ITweetRepository>();
+
+            var tweetId = "30";
+            var tweet = new Tweet() { Text = "Netflix&Chill" };
+            var expectedResult = new TweetDto() { Text = "Netflix&Chill" };
+
+            tweetRepositoryMock
+                .Setup(x => x.Get(tweetId))
+                .ReturnsAsync(tweet);
+
+            mappingProviderMock
+                .Setup(x => x.MapTo<TweetDto>(tweet))
+                .Returns(expectedResult);
+
+            var tweetService = new TweetService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                tweetRepositoryMock.Object);
+
+            //Act
+            var actualResult = await tweetService.GetTweetById(tweetId);
+
+            //Assert
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual(actualResult.Text, expectedResult.Text);
+        }
+    }
+}
---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/TwitterAccountServiceTests/AddTwitterAccountToUser_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.TwitterAccountServiceTests
+{
+    class AddTwitterAccountToUser_Should
+    {
+    }
+}

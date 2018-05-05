1ac71b32bd9cc4bf266afbbbb271169f974289d5
Alek Hristov <alek.hristov@yahoo.com>
2018-05-05 16:43:06 +0300

    Stage Ignored files to Source Control

---
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/GetUserByScreenName_Should.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
+{
+   [TestClass]
+    public class GetUserByScreenName_Should
+    {
+        [TestMethod]
+    }
+}

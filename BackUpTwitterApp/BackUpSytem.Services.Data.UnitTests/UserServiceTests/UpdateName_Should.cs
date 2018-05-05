7172d5ca3be0f805901a3dd9f68cdc1059c1fdf6
Alek Hristov <alek.hristov@yahoo.com>
2018-05-05 19:34:29 +0300

    UserService UpdateUserNameParameter Method tested

--- BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/UpdateName_Should.cs
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/UpdateName_Should.cs
@@ -7,8 +7,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
@@ -16,6 +14,68 @@ namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
     [TestClass]
     public class UpdateName_Should
     {
+        [TestMethod]
+        public async Task ReturnTrue_WhenNameSuccessfullyUpdated()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateName(userId, userName);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task ReturnFalse_WhenUserRepositoryUpdateUserNameMethodReturnsFalse()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(false);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateName(userId, userName);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
         [TestMethod]
         public async Task CallSaveChangesOnce_WhenInvokedWithValidParameters()
         {
@@ -31,7 +91,7 @@ namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
 
             userRepositoryMock
                 .Setup(x => x.UpdateName(userId, userName))
-                .Verifiable();
+                .ReturnsAsync(true);
 
             var userService = new UserService(
                 unitOfWorkMock.Object,
@@ -41,10 +101,153 @@ namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
                 userManagerMock.Object);
 
             //Act   
-            await userService.UpdateName(userId, userName);
+            var result = await userService.UpdateName(userId, userName);
 
             //Assert
             unitOfWorkMock.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task NotCallSaveChanges_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(false);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateName(userId, userName);
+
+            //Assert
+            unitOfWorkMock.Verify(x => x.SaveChanges(), Times.Exactly(0));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullUserIdParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await userService.UpdateName(null, userName));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentException_WhenInvokedWithInvalidEmptyUserIdParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(async () => await userService.UpdateName(string.Empty, userName));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullUserNameParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await userService.UpdateName(userId, null));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentException_WhenInvokedWithInvalidEmptyUserNameParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var userName = "Marto Stamatov";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateName(userId, userName))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(async () => await userService.UpdateName(userId, string.Empty));
         }
 
         public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class

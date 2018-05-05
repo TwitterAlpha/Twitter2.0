6c22f6d712fcd22f52b1e9d70b6bb15d94341008
Alek Hristov <alek.hristov@yahoo.com>
2018-05-05 20:03:43 +0300

    UserService UpdateProfileImageUrl Method tested

--- BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/UpdateProfileImage_Should.cs
+++ BackUpTwitterApp/BackUpSytem.Services.Data.UnitTests/UserServiceTests/UpdateProfileImage_Should.cs
@@ -1,12 +1,262 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using BackUpSystem.Data.Models;
+using BackUpSystem.Data.Repositories.Contracts;
+using BackUpSystem.Services.Data;
+using BackUpSystem.Services.Data.Contracts;
+using BackUpSystem.Utilities.Contracts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace BackUpSytem.Services.Data.UnitTests.UserServiceTests
 {
     [TestClass]
     public class UpdateProfileImage_Should
     {
+        [TestMethod]
+        public async Task ReturnTrue_WhenNameSuccessfullyUpdated()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateProfileImage(userId, imageUrl);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public async Task ReturnFalse_WhenUserRepositoryUpdateimageUrlMethodReturnsFalse()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(false);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateProfileImage(userId, imageUrl);
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public async Task CallSaveChangesOnce_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateProfileImage(userId, imageUrl);
+
+            //Assert
+            unitOfWorkMock.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task NotCallSaveChanges_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(false);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act   
+            var result = await userService.UpdateProfileImage(userId, imageUrl);
+
+            //Assert
+            unitOfWorkMock.Verify(x => x.SaveChanges(), Times.Exactly(0));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullUserIdParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await userService.UpdateProfileImage(null, imageUrl));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentException_WhenInvokedWithInvalidEmptyUserIdParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(async () => await userService.UpdateProfileImage(string.Empty, imageUrl));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithInvalidNullimageUrlParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(async () => await userService.UpdateProfileImage(userId, null));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentException_WhenInvokedWithInvalidEmptyimageUrlParameter()
+        {
+            //Arrange
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var mappingProviderMock = new Mock<IMappingProvider>();
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var twitterServiceMock = new Mock<ITwitterService>();
+            var userManagerMock = MockUserManager<User>();
+
+            var userId = "30";
+            var imageUrl = "Cool picture";
+
+            userRepositoryMock
+                .Setup(x => x.UpdateImageUrl(userId, imageUrl))
+                .ReturnsAsync(true);
+
+            var userService = new UserService(
+                unitOfWorkMock.Object,
+                mappingProviderMock.Object,
+                userRepositoryMock.Object,
+                twitterServiceMock.Object,
+                userManagerMock.Object);
+
+            //Act && Assert
+            Assert.ThrowsExceptionAsync<ArgumentException>(async () => await userService.UpdateProfileImage(userId, string.Empty));
+        }
+
+        public static Mock<UserManager<TUser>> MockUserManager<TUser>() where TUser : class
+        {
+            var store = new Mock<IUserStore<TUser>>();
+            var mgr = new Mock<UserManager<TUser>>(store.Object, null, null, null, null, null, null, null, null);
+            mgr.Object.UserValidators.Add(new UserValidator<TUser>());
+            mgr.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+            return mgr;
+        }
     }
 }

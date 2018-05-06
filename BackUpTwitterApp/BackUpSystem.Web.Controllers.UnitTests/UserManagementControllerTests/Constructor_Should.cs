fc4d6f079a485ef4539a6e77ca8b732029f764ec
simeonovanton <djnoreaga@gmail.com>
2018-05-06 04:46:45 +0300

    tests on develop branch....

---
+++ BackUpTwitterApp/BackUpSystem.Web.Controllers.UnitTests/UserManagementControllerTests/Constructor_Should.cs
@@ -0,0 +1,45 @@
+﻿using BackUpSystem.Services.Data.Contracts;
+using BackUpSystem.Web.Areas.Admin.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSystem.Web.Controllers.UnitTests.UserManagementControllerTests
+{
+    [TestClass]
+    class Constructor_Should
+    {
+        [TestMethod]
+        public void CreateInstance_WhenInvokedWithValidParameters()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+            var twitterAccountServiceMock = new Mock<ITwitterAccountService>();
+            var tweetServiceMock = new Mock<ITweetService>();
+
+            //Act
+            var userManagementController = new UserManagementController
+                 (
+             userServiceMock.Object,
+             twitterAccountServiceMock.Object,
+             tweetServiceMock.Object
+                );
+
+            //Assert
+            Assert.IsNotNull(userManagementController);
+            Assert.IsInstanceOfType(userManagementController, typeof(UserManagementController));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenInvokedWithNullUserServiceArgument()
+        {
+            //Arrange
+            //var userServiceMock = new Mock<IUserService>();
+
+            //Act && Assert
+            Assert.ThrowsException<ArgumentNullException>(() => new StatisticsController(null));
+        }
+    }
+}
---
+++ BackUpTwitterApp/BackUpSystem.Web/Areas/Admin/Controllers/Contracts/IUserManagementController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackUpSystem.Web.Areas.Admin.Controllers.Contracts
+{
+    interface IUserManagementController
+    {
+    }
+}

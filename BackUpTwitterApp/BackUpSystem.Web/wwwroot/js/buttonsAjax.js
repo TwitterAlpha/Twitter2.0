54782dd0d1b06d481f2768019cc0087583359d5e
Alek Hristov <alek.hristov@yahoo.com>
2018-05-07 12:42:52 +0300

    Changed .gitignore

--- BackUpTwitterApp/BackUpSystem.Web/wwwroot/js/buttonsAjax.js
+++ BackUpTwitterApp/BackUpSystem.Web/wwwroot/js/buttonsAjax.js
@@ -1,42 +1,60 @@
-﻿$(function () {
-    $(".btn").click(function () {
-        const data = {
-            'id': $(this).attr("parameter1"),
-            'createdAt': $(this).attr("parameter2"),
-            'likesCount': $(this).attr("parameter3"),
-            'retweetsCount': $(this).attr("parameter4"),
-            'text': $(this).attr("parameter5"),
-            'tweetAuthor': $(this).attr("parameter6"),
-            'tweetUrl': $(this).attr("parameter7")
-        };
+﻿<script>
+    $(function () {
+        $('.btn').click(function () {
 
-        console.log(data);
-        const btnId = "#btn-" + data.id;
-        const messageId = "#message" + data.id;
-        let postUrl;
+            const data = {
+                'id': $(this).attr("parameter1"),
+                'name': $(this).attr("parameter2"),
+                'backgroundImage': $(this).attr("parameter3"),
+                'description': $(this).attr("parameter4"),
+                'followersCount': $(this).attr("parameter5"),
+                'followingCount': $(this).attr("parameter6"),
+                'imageUrl': $(this).attr("parameter7"),
+                'tweetsCount': $(this).attr("parameter8"),
+                'username': $(this).attr("parameter9"),
+                'isInFavorites': Boolean($(this).attr("parameter10"))
+            };
 
-        if (data.createdAt != undefined) {
-            postUrl = "/tweet/download";
-        }
-        else {
-            postUrl = "/tweet/delete";
-        }
-        console.log(postUrl);
-        $.ajax({
-            type: "POST",
-            contentType: "application/json; charset=utf-8",
-            url: postUrl,
-            data: JSON.stringify(data),
-            datatype: "json",
-            success: function (result) {
+            const buttonId = $(this).attr('id');
+            console.log(buttonId);
 
-                $(btnId).hide();
+            let postUrl;
+            const messageId = `#message-${data.id}`;
 
-                $(messageId).text(result).css({ 'color': 'green', 'font-weight': 'normal' }).show().delay(3000).fadeOut();
-            },
-            error: function (result) {
-                $(messageId).text("Error: " + result).css({ 'color': 'red', 'font-weight': 'normal' }).show().delay(3000).fadeOut();;
+            if (buttonId.startsWith('follow')) {
+                postUrl = '/twitteraccount/AddTwitterAccountToFavorites';
             }
+            else {
+                postUrl = '/twitteraccount/UnfollowTwitterAccount';
+            }
+            console.log(postUrl);
+
+            $.ajax({
+                type: "POST",
+                contentType: "application/json; charset=utf-8",
+                url: postUrl,
+                data: JSON.stringify(data),
+                datatype: "json",
+                success: function (result) {
+
+                    if (buttonId.startsWith('follow')) {
+                        console.log('follow');
+                        $(`#${buttonId}`).css('visibility', 'hidden');
+                        $(`#unfollow-${data.id}`).css('visibility', 'visible');
+                    }
+                    else {
+                        console.log('unfollow');
+                        $(`#${buttonId}`).css('visibility', 'hidden');
+                        $(`#follow-${data.id}`).css('visibility', 'visible');
+                    }
+
+                    $(messageId).text(result).css({ 'color': 'green', 'font-weight': 'normal' }).show().delay(3000).fadeOut();
+                    $('#sidebar').load("/Home/UpdateFavoriteUsersInLayout");
+                },
+                error: function (result) {
+                    $(messageId).text("Error: " + result).css({ 'color': 'red', 'font-weight': 'normal' }).show().delay(3000).fadeOut();;
+                }
+            });
         });
     });
-});
+</script>

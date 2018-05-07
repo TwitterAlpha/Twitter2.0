3b12fcc1db3d4f0f5b303fbe85d2d961b4db1215
simeonovanton <djnoreaga@gmail.com>
2018-05-07 12:37:44 +0300

    more BackUpSystem.Web.Controllers.UnitTests

---
+++ BackUpTwitterApp/BackUpSystem.Web.Controllers.UnitTests/HomeControllerTests/Search_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSystem.Web.Controllers.UnitTests.HomeControllerTests
+{
+    class Search_Should
+    {
+    }
+}
---
+++ BackUpTwitterApp/BackUpSystem.Web.Controllers.UnitTests/SearchControllerTests/Constructor_Should.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackUpSystem.Web.Controllers.UnitTests.SearchControllerTests
+{
+    class Constructor_Should
+    {
+    }
+}
